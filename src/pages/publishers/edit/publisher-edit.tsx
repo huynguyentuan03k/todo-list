12fafb4153bd25e781de27a5108e5c576b2568a5
Tuan Huy <tuanhuy16903@gmail.com>
2025-09-24 21:45:39 +0700

    react hook form , register, watch, setValue, formState

--- src/pages/publishers/edit/publisher-edit.tsx
+++ src/pages/publishers/edit/publisher-edit.tsx
@@ -10,37 +10,45 @@ import { useQuery } from "@tanstack/react-query"
 import http from "@/utils/http"
 import { Publisher, PublisherSchema } from "../shema"
 import { SpinnerLoading } from "@/pages/components/custom/SpinnerLoading"
-import { useForm, SubmitHandler } from "react-hook-form"
-
+import { useForm, } from "react-hook-form"
 
 export default function PublisherEdit() {
   const { id } = useParams()
   const navigate = useNavigate()
 
-  const getPublisher = (id: number) => http.get<{ data: Publisher }>(`/publishers/${id}`);
-
   const { data, isLoading } = useQuery({
-    queryKey: ['publisher'],
-    queryFn: () => getPublisher(Number(id))
+    queryKey: ['publisher', id],
+    queryFn: () => http.get<{ data: Publisher }>(`/publishers/${id}`)
   })
 
-  // React Hook Form
-  const { register, handleSubmit, setValue, watch, formState: { errors } } = useForm<Publisher>()
-
-  const onSubmit: SubmitHandler<Publisher> = (data) => console.log(data)
 
   if (isLoading) {
-    return <SpinnerLoading />
+    <SpinnerLoading />
   }
 
   const publisher = PublisherSchema.parse(data?.data.data)
 
 
-  // Mutation update
+  // React Hook Form , register dang ky filed voi form, handleSubmit dung de validation truoc khi goi callback
+  const { register, handleSubmit, setValue, watch, formState: { errors } } = useForm<Publisher>({
+    defaultValues: {
+      name: publisher.name,
+      email: publisher.email,
+      address: publisher.address,
+      website: publisher.website,
+      phone: publisher.phone
+    }
+  })
+
+  // const onSubmit: SubmitHandler<Publisher> = (data) => console.log(data)
+  function onSubmit(data: Publisher) {
+    console.log("data ", data)
+  }
 
 
+  const newName = watch('name')
   return (
-    <div>
+    <div >
       <div className="flex justify-end">
         <Button className="bg-blue-500 text-white hover:bg-blue-600" onClick={() => navigate(-1)}>Back</Button>
       </div>
@@ -49,40 +57,47 @@ export default function PublisherEdit() {
           <CardTitle>Edit Publisher</CardTitle>
           <CardDescription>description Edit Publisher</CardDescription>
         </CardHeader>
-        <form onSubmit={handleSubmit(onSubmit)} >
+        <form onSubmit={handleSubmit(data => onSubmit(data))} >
           <CardContent>
             <div className="grid grid-cols-3 gap-4">
 
               <div className="flex flex-col col-span-1">
-                <Label htmlFor="name" >Name</Label>
-                <Input id="name" {...register} defaultValue={publisher.name} placeholder="Name of your publisher" />
+                <Label htmlFor="name" >Name : {newName}</Label>
+                {errors.name && <span>{errors.name.message}</span>}
+                <Input id="name" {...register('name', { required: "ten ko de trong" })} placeholder="Name of your publisher" />
               </div>
 
               <div className="flex flex-col col-span-1">
                 <Label htmlFor="address" >Address</Label>
-                <Input id="address" placeholder="address of your" />
+                <Input {...register('address')} id="address" placeholder="address of your" />
               </div>
 
               <div className="flex flex-col col-span-1">
                 <Label htmlFor="email" >Email</Label>
-                <Input id="email" placeholder="email of your" />
+                <Input {...register("email", {
+                  required: "Email là bắt buộc",
+                  pattern: { value: /^\S+@\S+$/i, message: "Email không hợp lệ" }
+                })} />
+                {errors.email && <span>{errors.email.message}</span>}
               </div>
 
               <div className="flex flex-col col-span-1">
                 <Label htmlFor="website" >Website</Label>
-                <Input id="website" placeholder="website of your" />
+                <Input {...register('website')} id="website" placeholder="website of your" />
               </div>
 
               <div className="flex flex-col col-span-1">
                 <Label htmlFor="phone" >Phone</Label>
-                <Input id="phone" placeholder="phone of your" />
+                <Input {...register('phone')} id="phone" placeholder="phone of your" />
               </div>
-              <div className="flex flex-col col-span-1">
 
+              {/* phone */}
+              <div className="flex flex-col col-span-1">
                 <Label htmlFor="phone">Phone</Label>
                 <PhoneInput />
               </div>
 
+              {/* created_at */}
               <div className="flex flex-col col-span-1">
                 <Label htmlFor="created_at" >Created At</Label>
                 <DatePicker value={publisher.created_at} />
@@ -91,10 +106,10 @@ export default function PublisherEdit() {
             </div>
           </CardContent>
           <CardFooter className="flex justify-end">
-            <Button type="submit" className="bg-blue-500 text-white hover:bg-blue-600" onClick={() => navigate(-1)}>Save</Button>
+            <Button type="submit" className="bg-blue-500 text-white hover:bg-blue-600">Save</Button>
           </CardFooter>
         </form>
       </Card>
-    </div >
+    </div>
   )
 }
