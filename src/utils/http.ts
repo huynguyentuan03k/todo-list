17e55db30553d2a292b4f6e69e9dda3205b4322d
Tuan Huy <tuanhuy16903@gmail.com>
2025-12-16 12:36:42 +0700

    change const env

--- src/utils/http.ts
+++ src/utils/http.ts
@@ -4,7 +4,8 @@ class Http {
   instance: AxiosInstance;
   constructor() {
     this.instance = axios.create({
-      baseURL: import.meta.env.BASE_URL || 'http://localhost:8000/api/',
+      baseURL: import.meta.env.VITE_API_BASE_URL || 'http://localhost:8000/api/',
+
       timeout: 10000,
       headers: {
         'Content-Type': 'application/json',
