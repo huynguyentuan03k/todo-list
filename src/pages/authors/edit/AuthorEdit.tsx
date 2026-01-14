68eb46874906ca326c720c797f232e3fdad2efa6
Tuan Huy <tuanhuy16903@gmail.com>
2026-01-14 22:59:01 +0700

    responsive autho edit

--- src/pages/authors/edit/AuthorEdit.tsx
+++ src/pages/authors/edit/AuthorEdit.tsx
@@ -114,9 +114,10 @@ export default function AuthorEdit() {
         </CardHeader>
         <form onSubmit={form.handleSubmit(data => onSubmit(data))} >
           <CardContent>
-            <div className="grid grid-cols-3 gap-4">
+            <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-4">
 
               <Form {...form}>
+
 
                 <FormField
                   control={form.control}
