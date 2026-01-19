1d6c4c81887e12a5f97d17b9efe151a69852ab49
Tuan Huy <tuanhuy16903@gmail.com>
2026-01-19 20:42:22 +0700

    add init language for editer

--- src/pages/components/custom/TinyEditor.tsx
+++ src/pages/components/custom/TinyEditor.tsx
@@ -7,15 +7,22 @@ export default function TinyEditor() {
       licenseKey="gpl"
       init={{
         height: 600,
-        menubar: true,
+        // menubar: true,
         plugins: [
-          "advlist", "autolink", "lists", "link", "image",
-          "charmap", "preview", "searchreplace", "visualblocks",
-          "code", "fullscreen", "insertdatetime", "media",
-          "table", "help", "wordcount"
+          'advlist', 'autolink', 'link', 'image', 'lists', 'charmap', 'preview', 'anchor', 'pagebreak',
+          'searchreplace', 'wordcount', 'visualblocks', 'visualchars', 'code', 'fullscreen', 'insertdatetime',
+          'media', 'table', 'emoticons', 'help'
         ],
-        toolbar:
-          "undo redo | blocks | bold italic | alignleft aligncenter alignright | bullist numlist | code fullscreen",
+        toolbar: 'undo redo | styles | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image | print preview media | forecolor backcolor emoticons',
+        menu: {
+          favs: { title: 'My Favorites Huy', items: 'code visualaid | searchreplace | emoticons' }
+        },
+        menubar: 'favs file edit view insert format tools table help',
+
+        // language
+        // language: 'vi',
+        // language_url: '/tinymce/langs/vi.js',
+        // language_load: true,
 
         valid_elements: "*[*]",
         extended_valid_elements: "script[*],style[*]",
