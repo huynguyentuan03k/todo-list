03c6d54f27495306ad7a1eb0bec2b91b76a77c4c
Tuan Huy <tuanhuy16903@gmail.com>
2026-02-06 15:19:36 +0700

    fix bug : memory leak because when click create multiple file object audio

--- src/pages/components/custom/ValidationUrlAudio.tsx
+++ src/pages/components/custom/ValidationUrlAudio.tsx
@@ -1,6 +1,6 @@
 import { Button } from "@/components/ui/button";
 import { Textarea } from "@/components/ui/textarea";
-import { useState } from "react";
+import { useRef, useState } from "react";
 
 type Props = {
   url: string
@@ -9,6 +9,9 @@ type Props = {
 
 export default function ValidationUrlAudio({ url, onChange }: Props) {
   const [status, setStatus] = useState<"idle" | "loading" | "valid" | "invalid">('idle')
+  const [duration, setDuration] = useState<number>(0)
+  const audioRef = useRef<HTMLAudioElement | null>(null)
+
 
   const validateUrl = async () => {
     // thể fetch dữ liệu và đợi phẩn hồi đc vì CORS phải dùng thủ thuật này
@@ -21,24 +24,30 @@ export default function ValidationUrlAudio({ url, onChange }: Props) {
 
       setStatus('loading')
 
+
+      const audio = new Audio()
+
       const checkAudioPromise = new Promise<'valid' | 'invalid'>((resolve) => {
-        const audio = new Audio()
+        // chỉ có 1 đối tượng khi gọi hàm checkAudioPromise là audio còn audioRef.current chỉ lưu địa chỉ của đối tượng audio thôi
+        // useRef kiểm soát lifecycle và cleanup
+        // audioRef.current = audio
+
         audio.src = url
 
         audio.preload = 'metadata'
 
         audio.onloadedmetadata = () => {
+          console.log((Math.ceil(audio?.duration ?? 0)))
+          setDuration((audio?.duration ?? 0 / 60))
           resolve('valid')
-          audio.src = ''
         }
 
         audio.onerror = () => {
           resolve('invalid')
-          audio.src = ''
         }
 
       })
-
+      console.log("re-render")
       // const fetchPromise = fetch(url, {
       //   // dùng head để kiểm tra sự tồn tại của Content-Type
       //   // head gần giống với get nhưng nó chỉ trả về header của 1  response ko trả về body
@@ -51,10 +60,15 @@ export default function ValidationUrlAudio({ url, onChange }: Props) {
       // setStatus(result[0])
       const [result] = (await Promise.all([checkAudioPromise]));
 
+      audio.src = ''
+      audio.pause();
+      audio.load();
+
       setStatus(result)
 
     } catch (error) {
       setStatus('invalid')
+      console.error(error)
     }
 
   }
@@ -76,6 +90,7 @@ export default function ValidationUrlAudio({ url, onChange }: Props) {
         onClick={validateUrl}
       >Check</Button>
       <span className="ml-4">{status}</span>
+      <span className="ml-4">phút {duration === 0 ? 0 : duration}</span>
     </>
   )
 }
